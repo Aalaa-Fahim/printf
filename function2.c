29384426d964c17b576c2cd6f125fd9098a1294f
Aalaa-Fahim <alaahussin.322001@gmail.com>
2023-10-21 00:32:51 +0300

    Updated functions

--- function2.c
+++ function2.c
@@ -1,73 +1,82 @@
 #include "main.h"
 
 /**
- * count_digits - returns the number of digits in an integer
- * @n: integer to be evaluated
- *
- * Return: the number of digits
- */
-int count_digits(int n)
-{
-	unsigned int u, d = 0;
-
-	if (n < 0)
-		u = n * -1;
-	else
-		u = n;
-	while (u != 0)
-	{
-		u /= 10;
-		d++;
-	}
-	return (d);
-}
-
-/**
- * print_binary - prints a binary
+ * print_int - prints an integer
  * @l: a variable argument list
  * @p: pointer to the struct flags
  *
  * Return: number of nums printed
  */
-int print_binary(va_list l, flags_t *p)
+int print_int(va_list l, flags_t *p)
 {
-	unsigned int n = va_arg(l, unsigned int);
-	char *str = convert(n, 2, 0);
+	int n = va_arg(l, int);
+	int d = count_digits(n);
+
+	if (p->space == 1 && p->plus == 0 && n >= 0)
+		d += _putchar(' ');
+	if (p->plus == 1 && n >= 0)
+		d += _putchar('+');
+	if (n <= 0)
+		d++;
+	print_num(n);
+	return (d);
+}
+
+/**
+ * print_unsigned - prints an unsigned integer
+ * @l: a variable argument list
+ * @p: pointer to the struct flags
+ *
+ * Return: number of chars printed
+ */
+int print_unsigned(va_list l, flags_t *p)
+{
+	unsigned int ui = va_arg(l, unsigned int);
+	char *str = convert(ui, 10, 0);
 
 	(void)p;
 	return (_puts(str));
 }
 
 /**
- * print_non_printable - prints non printable characters
- * @l: a variable argument list
- * @p: pointer to the struct flags
- *
- * Return: number of nums printed
+ * print_num - loops through an integer and prints all its digits
+ * @n: integer to be printed
  */
-int print_non_printable(va_list l, flags_t *p)
+void print_num(int n)
 {
-	int i, c = 0;
-	char *hex;
-	char *s = va_arg(l, char *);
+	unsigned int n0;
 
-	(void)p;
-	if (!s)
-		return (_puts("(null)"));
-
-	for (i = 0; s[i]; i++)
+	if (n < 0)
 	{
-		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
-		{
-			_puts("\\x");
-			c += 2;
-			hex = convert(s[i], 16, 0);
-			if (!hex[1])
-				c += _putchar('0');
-			c += _puts(hex);
-		}
-		else
-			c += _putchar(s[i]);
+		_putchar('-');
+		n0 = -n;
 	}
-	return (c);
+	else
+		n0 = n;
+
+	if (n0 / 10)
+		print_num(n0 / 10);
+	_putchar((n0 % 10) + '0');
+}
+
+/**
+ * count_digits - returns the number of digits in an integer
+ * @n: integer to be evaluated
+ *
+ * Return: the number of digits
+ */
+int count_digits(int n1)
+{
+	unsigned int u, d = 0;
+
+	if (n1 < 0)
+		u = n1 * -1;
+	else
+		u = n1;
+	while (u != 0)
+	{
+		u /= 10;
+		d++;
+	}
+	return (d);
 }
